80fc710e2429ac0f145d36e981210d4e98d91135
HeltonDeus <>
2021-11-13 00:28:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+let driverName = 'John'; /*1.1*/
+console.log(`The driver's name is ${driverName}.`); /*1.2*/
+
+let navigatorName ='Paul'; /*1.3*/
+console.log(`The navigator's name is ${navigatorName}.`); /*1.4*/
+
+let driverCharacters === driverName.length();  /*
+let navigatorCharacters === navigatorName.length();
+
+if (driverCharacters > navigatorCharacters){
+  console.log(`The driver has the longest name, it has ${driverName.length()}.`);
+
+}else if (navigatorCharacters > driverCharacters) {
+  console.log(`It seems that the navigator has the longest name, it has ${navigatorName.length()}.`);
+
+}else if (navigatorCharacters = driverCharacters){
+  console.log(`Wow, you both have equally long names, ${driverName.length()}.`);
+
+} else {
+  console.log (`The amount of the characters is unknown.`);
+}
+
+
+
+
